64ecaa8cc40d1ad2bf509600e563ab085414ac30
naila25 <nailatorres716@gmail.com>
2025-09-24 23:07:17 -0600

    modificacion en talleres

--- src/modules/Landing/Pages/HomePage.tsx
+++ src/modules/Landing/Pages/HomePage.tsx
@@ -3,7 +3,7 @@ import AboutSection from "../Components/AboutSection";
 //import Testimonials from "../Components/Testimonials";
 import VolunteersSection from "../../Volunteers/Components/VolunteersSection";
 import Donaciones from "../../Donation/Components/Donaciones";
-import PublicWorkshopsPage from "../../Workshops/Pages/PublicWorkshopsPage";
+import PublicWorkshopsPage from "../../Workshops/Components/PublicWorkshopsPage";
 //import EventsNews from "../../EventsNews/Components/EventsNews";
 import LocationMap from "../Components/LocationMap";
 import HistoriasdeVida from "../Components/HistoriasdeVida"; 
