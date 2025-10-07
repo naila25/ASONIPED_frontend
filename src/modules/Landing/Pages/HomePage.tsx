ecf4aa792224048fb4b1509a04d9045a1866fc92
SyKarma <102105983+SyKarma@users.noreply.github.com>
2025-10-07 09:36:33 -0600

    [Minor Changes/Home Page and AboutSection]

--- src/modules/Landing/Pages/HomePage.tsx
+++ src/modules/Landing/Pages/HomePage.tsx
@@ -13,27 +13,11 @@ const Home = () => {
     <>
          <HeroSection />
          <AboutSection />
-      <div className="max-w-7xl mx-auto pt-10 px-6">
-        <div className="space-y-8 pt-10">
-          
-          <DonationSection /> {/*Prioridad a Donaciones por el Sprint 0 */}
-          {/*<PublicWorkshopsPage />*/} {/*Taller de voluntariado, por ahora no se implementa*/}
-          {/*<EventsNews />*/}
-          {/*<Testimonials />*/}
-        </div>
-      </div>
-      <PublicWorkshopsPage />
-
-      <div className="max-w-7xl mx-auto px-6 space-y-8 pt-10">
-        <HistoriasdeVida /> 
-      </div> 
-
-      {/* Voluntariado fuera para que sea full width */}
-      <VolunteersSection />
-
-      <div className="max-w-7xl mx-auto px-6 space-y-8 pt-10">
-        <LocationMap />
-      </div>
+         <DonationSection />
+         <PublicWorkshopsPage />
+         <HistoriasdeVida /> 
+         <VolunteersSection />
+         <LocationMap />
     </>
   );
 };
