3db8e8d794e448dda604a4332d7cf2f0b79a377c
naila25 <nailatorres716@gmail.com>
2026-03-02 20:10:16 -0600

    cambios, en movil

--- src/modules/Donation/Components/FormularioDonacion.tsx
+++ src/modules/Donation/Components/FormularioDonacion.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { FaMoneyBillWave, FaGift, FaMobileAlt, FaUniversity } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import quienessomos from "../../../assets/quienessomos.png";
@@ -110,35 +109,26 @@ const DonacionesVisual = () => {
     <section className="relative min-h-screen bg-white px-6 py-10 flex flex-col items-center gap-3">
 
       {/* Main title */}
-      <motion.h2
+      <h2
         className="text-orange-600 text-4xl font-semibold text-center mb-4 tracking-wide"
-        initial={{ opacity: 0, y: -50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.2 }}
+        
       >
         ¿Por qué donar?
-      </motion.h2>
+      </h2>
 
-      <motion.p
+      <p
         className="text-neutral-700 max-w-3xl text-center mb-6 text-lg"
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.1 }}
-        viewport={{ once: true, amount: 0.2 }}
+        
       >
         Cada donación que recibimos es una oportunidad para transformar vidas. En ASONIPED trabajamos día a día
         para brindar apoyo a personas con discapacidad y a sus familias. Tu ayuda nos permite ofrecer talleres,
         entregas de víveres, asistencia técnica y mucho más.
-      </motion.p>
+      </p>
 
       {/* DONATION METHODS */}
-      <motion.h3
+      <h3
         className="text-orange-600 text-3xl font-semibold text-center mb-4 tracking-wide"
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.1 }}
-        viewport={{ once: true, amount: 0.2 }}
+        
       >
         Formas de donación
 
@@ -147,16 +137,13 @@ const DonacionesVisual = () => {
          <img src={manoscoloridas} alt="Logo manos" className="w-12 h-12 object-contain" />
           <div className="w-20 h-[2px] bg-orange-500 rounded"></div>
         </div>
-      </motion.h3>
+      </h3>
 
       <div className="flex flex-col gap-15 w-full max-w-6xl">
         {/* Economic Donation */}
-        <motion.div
+        <div
           className="flex flex-col md:flex-row items-center bg-white rounded-xl shadow-md overflow-hidden border border-gray-200"
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          viewport={{ once: true, amount: 0.2 }}
+         
         >
           <div className="w-full md:w-1/2 p-6">
             <div className="flex items-center mb-4">
@@ -180,15 +167,12 @@ const DonacionesVisual = () => {
               className="object-cover w-full h-full rounded-3xl"
             />
           </div>
-        </motion.div>
+        </div>
 
         {/* In-Kind Donation */}
-        <motion.div
+        <div
           className="flex flex-col md:flex-row-reverse items-center bg-white rounded-xl shadow-md overflow-hidden border border-gray-200"
-          initial={{ opacity: 0, x: -100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          viewport={{ once: true, amount: 0.2 }}
+        
         >
           <div className="w-full md:w-1/2 p-6">
             <div className="flex items-center mb-4">
@@ -206,16 +190,13 @@ const DonacionesVisual = () => {
               className="object-cover w-full h-full rounded-3xl"
             />
           </div>
-        </motion.div>
+        </div>
       </div>
 
       {/* FORM + FREQUENTLY ASKED QUESTIONS */}
-      <motion.div
+      <div
         className="w-full max-w-6xl bg-white border border-gray-200 rounded-xl shadow-xl p-10 mb-12 mt-16"
-        initial={{ opacity: 0, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut" }}
-        viewport={{ once: true, amount: 0.2 }}
+    
       >
 
         <h2 className="text-center text-3xl font-extrabold text-orange-600 mb-12">
@@ -443,7 +424,7 @@ const DonacionesVisual = () => {
             </button>
                       </form>
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 };
